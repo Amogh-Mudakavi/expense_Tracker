c5a0f413c2f84c0864935d641763cf060c4e844e
Aspiron zx <mudakavirekha4@gmail.com>
2024-11-27 14:57:18 +0530

    added

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_expense',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',  # Ensure this is 'localhost'
+        'PORT': '3306', 
     }
 }
 
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path , include
+from . import views
 
 
 urlpatterns = [
-    path('' , include('tracker.urls')),
-    path('admin/' , admin.site.urls),
+ 
+ path('' , views.index  , name = "index")
 ]
 
